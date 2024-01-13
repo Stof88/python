b8a4026f0cf74ad6bfdf058732f1b9063dbe85c9
Stof88 <koffeinea5s5@gmail.com>
2024-01-13 11:37:57 +0200

    this is a first lesson

---
+++ Lesson1/main.py
@@ -0,0 +1,15 @@
+print("Hello world")
+a="Lviv"
+name="Vitalii"
+age="05.08.88"
+# print(age)
+# if name=="Vitalii":
+    # print("Hi")
+number1 = 25
+number2 = 20
+if number1 < number2:
+    print("number1 is less then number2")
+elif number1 > number2:
+    if type(number1)==int and type(number2)==int:
+        print("це цілі числа")
+    print("number2 is less then number1")
